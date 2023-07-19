c614bd2d87ec5922b49be3333a324db3e95429db
GaryYa0 <ygy3389@gmail.com>
2023-07-19 17:25:23 +1000

    config swagger & post task/task schema swagger (#19)

--- server/src/controllers/tasks.controller.js
+++ server/src/controllers/tasks.controller.js
@@ -1,8 +1,53 @@
 const TaskModel = require('../models/task.model')
 
+
+/**
+ * @swagger
+ * /api/v1/tasks:
+ *   post:
+ *     summary: Create a new task
+ *     description: Create a new task and save it to the database.
+ *     tags:
+ *       - Tasks
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               parent_project:
+ *                 type: string
+ *                 description: The ID of the parent project to which the task belongs.
+ *               title:
+ *                 type: string
+ *                 description: The title of the task.
+ *               created_by:
+ *                 type: string
+ *                 description: The ID of the user who created the task. Should be obtained from req.user.id.
+ *     responses:
+ *       '201':
+ *         description: The newly created task object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       '400':
+ *         description: Bad request. The request body was invalid or the task could not be saved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A description of the error that occurred.
+ */
+
+
 const postTask = async (req, res) => {
   // get userId from req.body for testing purpose, should be req.user.id
-  const { parent_project, title, created_by  } = req.body
+  const { parent_project, title, created_by } = req.body
   // const created_by = req.user.id
   const task = new TaskModel({
     parent_project,
@@ -16,6 +61,7 @@ const postTask = async (req, res) => {
     res.status(400).json({ message: error.message })
   }
 }
+
 
 const getAllTasks = async(req, res) => {
   const tasks = await TaskModel.find().exec()
--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const v1Router = require('../routes')
 const config = require('../config')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('../utils/swagger')
 
 
 module.exports = async (app) => {
@@ -12,6 +14,11 @@ module.exports = async (app) => {
   app.use(morgan('dev'))
   app.use(bodyParser.json())
   app.use(config.api.prefix, v1Router)
+
+  // health check api
+  app.get('/healthcheck', (request, response) => response.status(200).send({ message: 'healthy' }))
+  // swagger api docs
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc))
   // testing only
   app.get('/', (req, res) => {
     res.send('Hello, world!')
