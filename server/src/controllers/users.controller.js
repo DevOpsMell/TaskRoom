ef2cee0d846bf8351d0f9e03fc66c732b1d70786
RyanXu <ryx999@hotmail.com>
2023-07-29 08:51:10 +0930

    Kan 45 back-end get all and single user

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -2,8 +2,10 @@ const UserModel = require('../models/user.model')
 
 const postUser = async (req, res, next) => {
   // ↓↓↓↓↓↓↓↓↓↓↓↓↓Example code only↓↓↓↓↓↓↓↓↓↓↓↓↓
-  const user = new UserModel(req.body)
+  const { username, email, hashed_password } = req.body
   try {
+    const user = new UserModel(username, email, hashed_password )
+
     await user.save()
     res.status(201).json({ message: `${user.id} created successfully` })
   }
@@ -14,6 +16,31 @@ const postUser = async (req, res, next) => {
   // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 }
 
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await UserModel.find()
+    res.json(users)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user = await UserModel.findById(id)
+    if (!user) {
+      throw new NotFoundError(`User Id ${id} not found`)
+    }
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   postUser,
+  getAllUsers,
+  getUserById,
+  postUser
 }
--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { postUser } = require('../controllers/users.controller');
+const { postUser,  getAllUsers, getUserById, } = require('../controllers/users.controller');
 // const { validateUser } = require('../middleware/userValidation');
 
 const userRouter = express.Router();
 
 userRouter.post('/', postUser);
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', getUserById);
 
 module.exports = userRouter;
