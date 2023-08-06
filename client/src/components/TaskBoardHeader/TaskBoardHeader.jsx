8fb961609301398a7a72884e6530f8b190b4e4d3
Sebrina X <sebrinax.619@gmail.com>
2023-08-07 09:16:33 +1000

    header bar UI adjustment

--- client/src/components/TaskBoardHeader/TaskBoardHeader.jsx
+++ client/src/components/TaskBoardHeader/TaskBoardHeader.jsx
@@ -3,29 +3,48 @@ import { AppBar, Box, Toolbar, Typography, Button, Avatar } from '@mui/material'
 
 export default function TaskBoardHeader() {
 	return (
-		<Box sx={{ flexGrow: 1, display: 'flex' }}>
-			<AppBar position='static' sx={{ backgroundColor: 'transparent' }}>
-				<Toolbar>
-					<Typography
-						variant='h6'
-						component='div'
-						sx={{ flexGrow: 1, color: '#42526e' }}
-					>
-						<img
-							src='../../public/images/TR_logo.png'
-							alt='TR_Logo'
-							style={{ height: '40px', marginRight: '10px' }}
-						/>
-						TaskRoom
-					</Typography>
-					<Button sx={{ color: '#42526e' }}>Project</Button>
-					<Button sx={{ color: '#42526e' }}>Create</Button>
+		<AppBar
+			position='static'
+			sx={{
+				backgroundColor: 'transparent',
+				backdropFilter: 'blur(5px)',
+				elevation: '2',
+				borderBottom: 'none',
+			}}
+		>
+			<Toolbar sx={{ paddingLeft: 0, paddingRight: 0 }}>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'row',
+						flexGrow: 2,
+						alignItems: 'center',
+					}}
+				>
+					<Button>
+						<Avatar src='/images/TR_logo.png' alt='TR_Logo' variant='rounded' />
+						<Typography variant='h6' sx={{ ml: 1, color: '#42526e' }}>
+							TaskRoom
+						</Typography>
+					</Button>
+					<Box>
+						<Button sx={{ color: '#42526e' }}>Project</Button>
+						<Button sx={{ color: '#42526e' }}>Create</Button>
+					</Box>
+				</Box>
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+					}}
+				>
 					<Button sx={{ color: '#42526e' }}>Login</Button>
 					<Button>
 						<Avatar alt='User Avatar' src='/static/images/avatar/1.jpg' />
 					</Button>
-				</Toolbar>
-			</AppBar>
-		</Box>
+				</Box>
+			</Toolbar>
+		</AppBar>
 	)
 }
