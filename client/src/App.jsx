f9f82efeea1fc38326cd5791e81721ecc74de202
Sebrina X <sebrinax.619@gmail.com>
2023-08-12 10:31:25 +1000

    update Header Banner only shown on project page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 // import Demo from './components/Demo'
 import Router from './routes'
-import TaskBoardHeader from './components/TaskBoardHeader'
-
 
 function App() {
-  return (
-    <BrowserRouter>
-		<TaskBoardHeader />
-      {/* <h1>Task Room</h1>
+	return (
+		<BrowserRouter>
+			{/* <h1>Task Room</h1>
       <Demo /> */}
-      <Router />
-    </BrowserRouter>
-  )
+			<Router />
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
