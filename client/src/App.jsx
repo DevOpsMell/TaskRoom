7aa507dae8e95c0d397c7f919a1fbc3d4f03f9da
SebrinaX <91865397+SebrinaX@users.noreply.github.com>
2023-08-12 16:23:23 +1000

    updated TaskBoardHeader switch  on and off on the routes, updated TaskBoardHeader link to different pages with css (#50)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 // import Demo from './components/Demo'
-import Router from './routes'
+import AppRouter from './routes'
 
 function App() {
 	return (
 		<BrowserRouter>
 			{/* <h1>Task Room</h1>
       <Demo /> */}
-			<Router />
+			<AppRouter />
 		</BrowserRouter>
 	)
 }
--- client/src/pages/project/Project.jsx
+++ client/src/pages/project/Project.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Grid, Box } from '@mui/material'
 import TaskColumn from '../../components/Column'
-import TaskBoardHeader from '../TaskBoardHeader'
 
 const columns = [
 	'64cc4e846387ebfd6a25b558',
@@ -10,27 +9,24 @@ const columns = [
 ]
 
 function Project() {
-  return (
-    <>
-      <TaskBoardHeader />
-      <Box
-        sx={{
-          height: "900px",
-          backgroundImage:
-            'url("https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2386x1600/47f09f0e3910259568294477d0bdedac/photo-1576502200916-3808e07386a5.jpg")',
-          backgroundSize: "cover",
-        }}
-      >
-        <Grid container spacing={0}>
-          {columns.map((column) => (
-            <Grid item xs={12} sm={"auto"} key={column} sx={{ margin: "6px" }}>
-              <TaskColumn columnId={column} key={column} />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </>
-  )
+	return (
+		<Box
+			sx={{
+				height: '900px',
+				backgroundImage:
+					'url("https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2386x1600/47f09f0e3910259568294477d0bdedac/photo-1576502200916-3808e07386a5.jpg")',
+				backgroundSize: 'cover',
+			}}
+		>
+			<Grid container spacing={0}>
+				{columns.map((column) => (
+					<Grid item xs={12} sm={'auto'} key={column} sx={{ margin: '6px' }}>
+						<TaskColumn columnId={column} key={column} />
+					</Grid>
+				))}
+			</Grid>
+		</Box>
+	)
 }
 
 export default Project
