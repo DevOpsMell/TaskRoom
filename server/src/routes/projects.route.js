86c07d99fde3d141ee740a79ffee84019e805504
Jessie <yujiaxin2009@gmail.com>
2023-08-12 16:58:15 +1000

    fix eslint/ fix unit test

--- server/src/routes/projects.route.js
+++ server/src/routes/projects.route.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const {
-    postProject,
-    getAllProjects,
-    getProjectById,
-    updateProject,
-    deleteProjectById
+  postProject,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+  deleteProjectById
 } = require('../controllers/project.controller')
 
 const projectRouter = express.Router()
