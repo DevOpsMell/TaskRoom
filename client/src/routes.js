7d19a4a80ca4a9437b4fd8d0325f0282b508220e
SebrinaX <91865397+SebrinaX@users.noreply.github.com>
2023-08-15 22:22:38 +1000

    updated routes.js file update code (#56)

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,33 +1,37 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Project from './pages/project'
+import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import Project from './pages/project'
 import TaskBoardHeader from './pages/TaskBoardHeader'
 
-const WithHeaderComponent = ({ component }) => {
-	return (
-		<div>
-			<TaskBoardHeader />
-			{component}
-		</div>
-	)
-}
-
-const Home = () => {
-	return <div>Welcome to the homepage</div>
-}
+const Router = [
+	{ path: '/', component: HomePage, header: true },
+	{ path: '/register', component: RegisterPage },
+	{ path: '/login', component: LoginPage },
+	{ path: '/project', component: Project, header: true },
+]
 
 function AppRouter() {
 	return (
 		<Routes>
-			<Route
-				path='/project'
-				element={<WithHeaderComponent component={<Project />} />}
-			/>
-			<Route path='/register' element={<RegisterPage />} />
-			<Route path='/login' element={<LoginPage />} />
-			<Route path='/' element={<WithHeaderComponent component={<Home />} />} />
+			{Router.map(({ path, component: Component, header }) => (
+				<Route
+					key={path}
+					path={path}
+					element={
+						header ? (
+							<>
+								<TaskBoardHeader />
+								<Component />
+							</>
+						) : (
+							<Component />
+						)
+					}
+				/>
+			))}
 		</Routes>
 	)
 }
