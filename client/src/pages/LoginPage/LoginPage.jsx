16f6c76c76832a762dcdd4a453c2b59c1cae6876
caitlinyy <39151383+caitlinyy@users.noreply.github.com>
2023-08-21 22:27:50 +1000

    Kan 15 front end login register page card container (#57)
    * add container for login and register page
    
    * feature validation for login and register page

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
 
 const LoginPage = () => {
   const [formValues, setFormValues] = useState({
@@ -10,10 +11,48 @@ const LoginPage = () => {
     password: "",
   });
 
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (!formValues.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      newErrors.email = "Email is invalid";
+      valid = false;
+    } else {
+      newErrors.email = "";
+    }
+
+    if (!formValues.password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
   const handleOnSubmit = (event) => {
     event.preventDefault();
+    const isValid = validateForm();
+
+  if (isValid) {
     console.log("Form submitted");
-  };
+    // console.log("Form values:", formValues);
+  } 
+  // else {
+  //   console.log("Form validation failed");
+  // }
+};
 
   const handleOnChange = (event) => {
     const { name, value } = event.target;
@@ -29,52 +68,66 @@ const LoginPage = () => {
       justifyContent="center"
       alignItems="center"
       minHeight="100vh"
+      bgcolor="#ffffff"
     >
-      <form onSubmit={handleOnSubmit} style={{ width: "45%" }}>
-      <h2 style={{ textAlign: "center", fontSize:"45px" }}>Login</h2>
-        <TextField
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          onChange={handleOnChange}
-          name="email"
-          type="text"
-        />
-
-        <TextField
-          label="Password"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          onChange={handleOnChange}
-          name="password"
-          type="password"
-        />
-
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          fullWidth
-          style={{ margin: "25px auto" }}
-        >
-          Login
-        </Button>
-
-        <p style={{ textAlign: "center" }}>
-          <span>Not a member?</span>
-          <Button
-            component={Link}
-            to="/"
+      <Container
+        maxWidth="sm"
+        sx={{
+          borderRadius: "3px",
+          padding: "25px 40px",
+          boxShadow: "rgba(0,0,0,0.1) 0 0 10px",
+        }}
+      >
+        <form onSubmit={handleOnSubmit}>
+          <h2 style={{ textAlign: "center", fontSize: "45px" }}>Login</h2>
+          <TextField
+            label="Email"
             variant="outlined"
-            size="small"
-            style={{ marginLeft: "10px" }}
+            margin="normal"
+            fullWidth
+            onChange={handleOnChange}
+            name="email"
+            type="text"
+            error={Boolean(errors.email)}
+            helperText={errors.email}
+          />
+
+          <TextField
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={handleOnChange}
+            name="password"
+            type="password"
+            error={Boolean(errors.password)}
+            helperText={errors.password}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            fullWidth
+            style={{ margin: "25px auto" }}
           >
-            Register
+            Login
           </Button>
-        </p>
-      </form>
+
+          <p style={{ textAlign: "center" }}>
+            <span>Not a member?</span>
+            <Button
+              component={Link}
+              to="/register"
+              variant="outlined"
+              size="small"
+              style={{ marginLeft: "10px" }}
+            >
+              Register
+            </Button>
+          </p>
+        </form>
+      </Container>
     </Box>
   );
 };
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -3,6 +3,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
 
 const RegisterPage = () => {
   const [formValues, setFormValues] = useState({
@@ -11,10 +12,58 @@ const RegisterPage = () => {
     password: "",
   });
 
-  // 在 RegisterPage 组件内定义 handleOnSubmit 函数
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (!formValues.name) {
+      newErrors.name = "Name is required";
+      valid = false;
+    } else {
+      newErrors.name = "";
+    }
+
+    if (!formValues.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      newErrors.email = "Email is invalid";
+      valid = false;
+    } else {
+      newErrors.email = "";
+    }
+
+    if (!formValues.password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    } else if (formValues.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+      valid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
   const handleOnSubmit = (event) => {
     event.preventDefault();
-    console.log("Form submitted");
+    const isValid = validateForm();
+
+    if (isValid) {
+      console.log("Form submitted");
+      // console.log("Form values:", formValues);
+    } 
+    // else {
+    //   console.log("Form validation failed");
+    // }
   };
 
   const handleOnChange = (event) => {
@@ -31,56 +80,77 @@ const RegisterPage = () => {
       justifyContent="center"
       alignItems="center"
       minHeight="100vh"
+      bgcolor="#ffffff"
     >
-      
-      <form onSubmit={handleOnSubmit} style={{ width: "45%" }}>
-      <h2 style={{ textAlign: "center", fontSize:"45px" }}>Register</h2>
-        <TextField
-          label="Name"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          onChange={handleOnChange}
-          name="name"
-          type="text"
-        />
+      <Container
+        maxWidth="sm"
+        sx={{
+          borderRadius: "3px",
+          padding: "25px 40px",
+          boxShadow: "rgba(0,0,0,0.1) 0 0 10px",
+        }}
+      >
+        <form onSubmit={handleOnSubmit}>
+          <h2 style={{ textAlign: "center", fontSize: "45px" }}>Register</h2>
+          <TextField
+            label="Name"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={handleOnChange}
+            name="name"
+            type="text"
+            error={Boolean(errors.name)}
+            helperText={errors.name}
+          />
 
-        <TextField
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          onChange={handleOnChange}
-          name="email"
-          type="text"
-        />
+          <TextField
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={handleOnChange}
+            name="email"
+            type="text"
+            error={Boolean(errors.email)}
+            helperText={errors.email}
+          />
 
-        <TextField
-          label="Password"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          onChange={handleOnChange}
-          name="password"
-          type="password"
-        />
+          <TextField
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={handleOnChange}
+            name="password"
+            type="password"
+            error={Boolean(errors.password)}
+            helperText={errors.password}
+          />
 
-        <Button 
-        variant="contained" 
-        color="primary" 
-        type="submit" 
-        fullWidth 
-        style={{ margin: "25px auto" }}>
-          Register
-        </Button>
-        <p style={{ textAlign: "center" }}>
-        <span>Already a member?</span>
-        <Button component={Link} to="/login" variant="outlined" size="small" style={{ marginLeft: "10px" }}>
-          Login
-        </Button>
-      </p>
-      </form>
-      
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            fullWidth
+            style={{ margin: "25px auto" }}
+          >
+            Register
+          </Button>
+          <p style={{ textAlign: "center" }}>
+            <span>Already a member?</span>
+            <Button
+              component={Link}
+              to="/login"
+              variant="outlined"
+              size="small"
+              style={{ marginLeft: "10px" }}
+            >
+              Login
+            </Button>
+          </p>
+        </form>
+      </Container>
     </Box>
   );
 };
